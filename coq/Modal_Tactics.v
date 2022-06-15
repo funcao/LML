0f26439e34f46a7b0776a6bab06c5c13f77fa8a8
Paulo Torrens <paulotorrens@gnu.org>
2022-06-15 17:53:58 -0300

    Prove Löb's theorem in K4 + fixpoints

---
+++ coq/Modal_Tactics.v
@@ -0,0 +1,111 @@
+Require Import List Modal_Library Modal_Notations Deductive_System.
+Import ListNotations.
+
+Lemma modal_ax1:
+  forall (S: axiom -> Prop) g A B,
+  S (ax1 A B) ->
+  (S; g |-- A) -> (S; g |-- [! B -> A !]).
+Proof.
+  intros.
+  eapply Mp.
+  - apply Ax with (a := ax1 A B).
+    + assumption.
+    + reflexivity.
+  - assumption.
+Defined.
+
+Lemma modal_ax2:
+  forall (S: axiom -> Prop) g A B C,
+  S (ax2 A B C) ->
+  (S; g |-- [! A -> B -> C !]) ->
+  (S; g |-- [! A -> B !]) ->
+  (S; g |-- [! A -> C !]).
+Proof.
+  intros S g a b c H H1 H2.
+  assert (S; g |-- [! (a -> b -> c) -> (a -> b) -> a -> c !]).
+  - apply Ax with (a := ax2 a b c).
+    + assumption.
+    + reflexivity.
+  - assert (S; g |-- [! (a -> b) -> a -> c !]).
+    + eapply Mp.
+      * exact H0.
+      * assumption.
+    + eapply Mp.
+      * exact H3.
+      * assumption.
+Defined.
+
+Lemma modal_ax5:
+  forall (S: axiom -> Prop) g A B,
+  S (ax5 A B) ->
+  (S; g |-- [! A /\ B !]) -> (S; g |-- A).
+Proof.
+  intros.
+  assert (S; g |-- Implies (And A B) A).
+  - apply Ax with (a := ax5 A B).
+    + assumption.
+    + reflexivity.
+  - eapply Mp.
+    + apply H1.
+    + assumption.
+Defined.
+
+Lemma modal_ax6:
+  forall (S: axiom -> Prop) g A B,
+  S (ax6 A B) ->
+  (S; g |-- [! A /\ B !]) -> (S; g |-- B).
+Proof.
+  intros.
+  assert (S; g |-- Implies (And A B) B).
+  - apply Ax with (a := ax6 A B).
+    + assumption.
+    + reflexivity.
+  - eapply Mp.
+    + apply H1.
+    + assumption.
+Defined.
+
+Lemma modal_compose:
+  forall (S: axiom -> Prop) g A B C,
+  S (ax1 [! B -> C !] A) ->
+  S (ax2 A B C) ->
+  (S; g |-- [! A -> B !]) ->
+  (S; g |-- [! B -> C !]) ->
+  (S; g |-- [! A -> C !]).
+Proof.
+  intros S g a b c ?H ?H H1 H2.
+  assert (S; g |-- [! a -> b -> c !]).
+  - eapply modal_ax1.
+    + assumption.
+    + assumption.
+  - eapply modal_ax2.
+    + eassumption.
+    + exact H3.
+    + exact H1.
+Defined.
+
+Lemma modal_axK:
+  forall (S: axiom -> Prop) g A B,
+  S (axK A B) ->
+  (S; g |-- [! [](A -> B) !]) -> (S; g |-- [! []A -> []B !]).
+Proof.
+  intros.
+  eassert (S; g |-- Implies ?[X] ?[Y]).
+  - apply Ax with (a := axK A B).
+    + assumption.
+    + reflexivity.
+  - eapply Mp.
+    + apply H1.
+    + assumption.
+Defined.
+
+Lemma modal_axK4:
+  forall (S: axiom -> Prop) g A,
+  S (axK4 A) ->
+  (S; g |-- [! []A -> [][]A !]).
+Proof.
+  intros.
+  apply Ax with (a := axK4 A).
+  - assumption.
+  - reflexivity.
+Defined.
--- coq/exemplos.v
+++ coq/exemplos.v
@@ -1,18 +1,75 @@
-(*File to create tests. *)
-Require Import List Modal_Library Modal_Notations Deductive_System.
+Require Import List Modal_Library Modal_Notations Deductive_System Modal_Tactics.
 Import ListNotations.
-(* Page proof: 22 *)
 
-Theorem Lob:
+Definition fixed_point S g :=
+  forall f,
+  exists p,
+  (* TODO: we should improve the notation here! *)
+  (S; g |-- And (Implies p (f (Box p))) (Implies (f (Box p)) p)).
+
+Example Lob:
+  (* Löb's theorem is provable in K4 (assuming fixed points). *)
   forall P,
+  fixed_point K4 nil ->
   (K4; nil |-- [! []P -> P !]) ->
   (K4; nil |-- [! P !]).
 Proof.
-  intros.
-  admit.
-Admitted.
+  (* Step 1. *)
+  intros P FP H1.
+  (* Step 2. *)
+  destruct FP with (fun X => [! X -> P !]) as (psi, H2).
+  (* Step 3. *)
+  assert (K4; nil |-- [! psi -> []psi -> P !]) as H3.
+  apply modal_ax5 in H2; try repeat constructor.
+  exact H2.
+  (* Step 4. *)
+  assert (K4; nil |-- [! [](psi -> []psi -> P) !]) as H4.
+  apply Nec; auto.
+  (* Step 5. *)
+  assert (K4; nil |-- [! []psi -> []([]psi -> P) !]) as H5.
+  apply modal_axK; try repeat constructor.
+  exact H3.
+  (* Step 6. *)
+  assert (K4; nil |-- [! []([]psi -> P) -> [][]psi -> []P !]) as H6.
+  eapply Ax with (a := axK ?[X] ?[Y]); try repeat constructor.
+  (* Step 7. *)
+  assert (K4; nil |-- [! []psi -> [][]psi -> []P !]) as H7.
+  eapply modal_compose; try repeat constructor.
+  exact H5.
+  exact H6.
+  (* Step 8. *)
+  assert (K4; nil |-- [! []psi -> [][]psi !]) as H8.
+  apply modal_axK4; try constructor 2.
+  (* Step 9. *)
+  assert (K4; nil |-- [! []psi -> []P !]) as H9.
+  eapply modal_ax2; try repeat constructor.
+  exact H7.
+  exact H8.
+  (* Step 10. *)
+  assert (K4; nil |-- [! []psi -> P !]) as H10.
+  eapply modal_compose; try repeat constructor.
+  exact H9.
+  exact H1.
+  (* Step 11. *)
+  assert (K4; nil |-- [! ([]psi -> P) -> psi !]) as H11.
+  apply modal_ax6 in H2; try repeat constructor.
+  exact H2.
+  (* Step 12. *)
+  assert (K4; nil |-- psi) as H12.
+  eapply Mp.
+  exact H11.
+  exact H10.
+  (* Step 13. *)
+  assert (K4; nil |-- [! []psi !]) as H13.
+  apply Nec.
+  exact H12.
+  (* Step 14. *)
+  eapply Mp.
+  exact H10.
+  exact H13.
+Qed.
 
-Lemma Example:
+Example Ex1:
   (* TODO: fix notation for deduction! *)
   T; ([! [](#0 -> #1) !] :: [! [](#1 -> #2) !] :: nil) |-- [! [](#0 -> #2) !].
 Proof.
