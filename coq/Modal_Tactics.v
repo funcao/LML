c07d6effd6553f4e66886e3ee6b4294d50981028
MiguelANunes <miguel.alfredo.nunes@gmail.com>
2022-06-17 20:57:30 -0300

    Generalizando modal_impl_transitivity

--- coq/Modal_Tactics.v
+++ coq/Modal_Tactics.v
@@ -111,10 +111,10 @@ Proof.
 Defined.
 
 Lemma modal_impl_transitivity:
-  forall f v a b c,
-  ( ([f--v] |= [! a -> b !]) /\ ([f--v] |= [! b -> c !])) ->
-  (  [f--v] |= [! a -> c !]).
+  forall M a b c,
+  ( (M |= [! a -> b !]) /\ (M |= [! b -> c !])) ->
+  (  M |= [! a -> c !]).
 Proof.
-  intros f v a b c [H1 H2] w H3.
+  intros M a b c [H1 H2] w H3.
   apply H2; apply H1; assumption.
 Defined.
