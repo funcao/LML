ba3ecb00a98a2cd2c6b0157401e0474e8eee29be
Paulo Torrens <paulotorrens@gnu.org>
2023-06-18 12:32:55 +0100

    Add needed definitions for Lindebaum's lemma

--- coq/Completeness.v
+++ coq/Completeness.v
@@ -1,227 +1,134 @@
-Require Import Modal_Library Modal_Notations Deductive_System List Classical Bool.
+Require Import Modal_Library Modal_Notations Deductive_System Soundness List Classical Bool Sets.
 
-(*
+(* Assume that formulas are constructively countable for now. *)
+Axiom encode: formula -> nat.
+Axiom decode: nat -> option formula.
+Axiom countable: forall p, decode (encode p) = Some p.
+
+Section Properties.
+
+  Variable A: axiom -> Prop.
+  Variable G: theory.
+
+  Definition Consistent: Prop :=
+    forall p,
+    ~(A; G |-- [! p /\ ~p !]).
+
+  Definition Maximal: Prop :=
+    forall p,
+    G p \/ G [! ~p !].
+
+End Properties.
+
 Section Lindebaum.
 
-Definition Consistent (A: axiom -> Prop) (G : theory) : Prop :=
-  forall p,
-  ~(A; G |-- [! p /\ ~p !]).
+  (* For now, assume that the set of axioms is K. *)
+  Definition A: axiom -> Prop := K.
 
-Definition Maximal_Consistent (A: axiom -> Prop) (G : theory) : Prop :=
-  forall p,
-  ~(In p G /\ In [! ~p !] G) /\ Consistent A G.
+  (* Base theory. *)
+  Variable G: theory.
 
-Lemma lema_1:
-  forall A Delta Gamma,
-  (Consistent A Delta /\
-  subset Gamma Delta) ->
-  Consistent A Gamma.
-Proof.
-  intros.
-  destruct H.
-  unfold Consistent, not in *; intros.
-  eapply H;
-  eapply derive_weak.
-  exact H0.
-  exact H1.
-Qed.
+  (* Since Coq can't construct a type based on classical reasoning, turn this
+     into an inductive type by assuming either a formula or its negation. *)
+  Inductive Insert: option formula -> theory -> theory :=
+    (* If we don't have a formula, keep D. *)
+    | Insert_skip:
+      forall D,
+      Subset D (Insert None D)
 
-Variable P: nat -> formula.
-Variable Gamma: theory.
-Variable A: axiom -> Prop.
+    (* If our formula is sound, insert it. *)
+    | Insert_valid:
+      forall D p,
+      Consistent A (Union (Singleton p) D) ->
+      Subset (Union (Singleton p) D) (Insert (Some p) D)
 
-Inductive Lindenbaum_set : nat -> theory -> Prop :=
-  | Lindenbaum_zero:
-    Lindenbaum_set 0 Gamma
-  | Lindenbaum_succ1:
-    forall n Delta,
-    Lindenbaum_set n Delta ->
-    Consistent A (P n :: Delta) ->
-    Lindenbaum_set (S n) (P n :: Delta)
-  | Lindenbaum_succ2:
-    forall n Delta,
-    Lindenbaum_set n Delta ->
-    ~Consistent A (P n :: Delta) ->
-    Lindenbaum_set (S n) Delta.
+    (* Finally, if our formula is not sound, insert it's opposite. *)
+    | Insert_invalid:
+      forall D p,
+      ~Consistent A (Union (Singleton p) D) ->
+      Subset (Union (Singleton [! ~p !]) D) (Insert (Some p) D).
 
-Lemma construct_set: (*existe 1 conjunto de Lindenbaum*)
-  forall n,
-  exists Delta, 
-  Lindenbaum_set n Delta.
-Proof.
-  intros; induction n.
-  - exists Gamma.
-    constructor.
-  - destruct IHn as (Delta, ?H). 
-    edestruct classic with (Consistent A (P n :: Delta)).
-    + eexists.
-      apply Lindenbaum_succ1; eauto.
-    + eexists.
-      apply Lindenbaum_succ2; eauto.
-Qed.
+  (* Lindebaum sets. *)
+  Fixpoint Delta (n: nat): theory :=
+    match n with
+    | 0   => G
+    | S n => Insert (decode n) (Delta n)
+    end.
 
-(*
-  P/ todo conjunto de Lindenbaum Delta, Gamma é subconjunto dele
-*)
+  (* Maximal set. *)
+  Definition Max: theory :=
+    UnionOf Delta.
 
-Lemma Lindenbaum_subset: 
-  forall n Delta,
-  Lindenbaum_set n Delta -> 
-  subset Gamma Delta.
-Proof.
-  unfold subset; intros.
-  induction H.
-  - assumption.
-  - intuition.
-  - assumption.
-Qed.
+  Lemma subset_of_insert:
+    forall D p,
+    Subset D (Insert p D).
+  Proof.
+    admit.
+  Admitted.
+
+  Lemma delta_subset_of_max:
+    forall n,
+    Subset (Delta n) Max.
+  Proof.
+    admit.
+  Admitted.
+
+  Lemma gamma_subset_of_max:
+    Subset G Max.
+  Proof.
+    admit.
+  Admitted.
+
+  Lemma delta_is_monotonic:
+    forall n m,
+    n < m ->
+    Subset (Delta n) (Delta m).
+  Proof.
+    admit.
+  Admitted.
+
+  Lemma insert_contains_formula:
+    forall D p,
+    Insert (Some p) D [! p !] \/ Insert (Some p) D [! ~p !].
+  Proof.
+    admit.
+  Admitted.
+
+  Lemma max_is_maximal:
+    forall p,
+    Max [! p !] \/ Max [! ~p !].
+  Proof.
+    admit.
+  Admitted.
+
+  Lemma insert_preserves_consistency:
+    forall D p,
+    Consistent A D -> Consistent A (Insert p D).
+  Proof.
+    admit.
+  Admitted.
+
+  Lemma delta_is_consistent:
+    Consistent A G ->
+    forall n,
+    Consistent A (Delta n).
+  Proof.
+    admit.
+  Admitted.
+
+  Lemma max_is_consistent:
+    Consistent A G ->
+    Consistent A Max.
+  Proof.
+    admit.
+  Admitted.
+
+  Theorem lindebaum:
+    Consistent A G ->
+    exists D,
+    Consistent A D /\ Maximal D /\ Subset G D.
+  Proof.
+    admit.
+  Admitted.
 
 End Lindebaum.
-
-(*
-############################################
-############################################
-############################################
-*)
-
-(*
-  Delta 0 := Gamma
-  Delta (n + 1) := Delta n \union (phi n + 1)
-                      se consistente
-                   Delta n \union (~phi n + 1)
-                      senao
-
-  Delta = Union (n: N) Delta n
-
-*)
-
-Section Lindebaum'.
-
-(*Inductive deduction (A: axiom -> Prop): theory -> formula -> Prop*)
-(*Isso é um problema*)
-
-(* Print filter.
-Print reflect. *)
-
-(*
-É possível que não de para
-coverter Props para Bools de forma alguma,
-nesse caso, muda o Delta p/ uma função de bool e adapta o código
-de acordo com o necessário [?]
-
-Definition Consistent' (A: axiom -> Prop) (G : theory) : Prop := 
-  forall p,
-  ~ (A; G |-- [! p /\ ~p !]).
-
-Definition Maximal_Consistent' (A: axiom -> Prop) (G : theory) : Prop :=
-  forall p,
-  ~(In [! p !] G /\  In [! ~ p !] G) /\ Consistent A G.
-*)
-
-Variable P: nat -> formula. 
-Variable A: axiom -> Prop.
-Variable Gamma: formula -> Prop. (*X -> Prop == Conjunto de X*)
-(*Variable G: Consistent A Gamma. Essa definição não funciona com a formulação
-                                  atual de Gamma*)
-
-Inductive Singleton {T} (t: T): T -> Prop := (*conjunto de um único*)
-  | Singleton_mk: Singleton t t.             (*elemento*)
-
-Definition Union {T} (A B: T -> Prop): T -> Prop :=
-  fun t => A t \/ B t.
-
-Fixpoint Delta (n: nat): formula -> Prop := (*arrumar vc pra ver*)
-  match n with                                   (*consistencia*)
-  | 0   => Gamma
-  | S n => Union (Delta n) (Singleton (P n))
-  end.
-
-Definition Subset {T} (A B: T -> Prop): Prop :=
-  forall t: T,
-  A t -> B t.
-
-Goal
-  forall n,
-  Subset (Delta n) (Delta (S n)).
-Proof.
-  intros.
-  unfold Subset; intros.
-  simpl.
-  unfold Union.
-  left; assumption.
-Qed.
-
-Goal
-  forall n,
-  Delta (S n) (P n).
-Proof.
-  intros; simpl.
-  unfold Union.
-  right.
-  constructor.
-Qed.
-
-(*I é um tipo de indices, S é um conjunto de T's indexados por I's*)
-Inductive UnionOf {I} {T} (S: I -> T -> Prop): T -> Prop :=
-  | UnionOf_mk:
-    forall i: I,
-    Subset (S i) (UnionOf S).
-
-Definition MaxDelta: formula -> Prop :=  UnionOf Delta.
-
-Goal
-  forall n,
-  Subset (Delta n) MaxDelta.
-Proof.
-  intros.
-  unfold Subset; intros.
-  unfold MaxDelta.
-  apply UnionOf_mk with n.
-  assumption.
-Qed.
-
-Inductive FormulaSet: nat -> formula -> Prop := (*deixando vc aqi*)
-  | FLit: forall n m, m < n -> FormulaSet n (P m)    (*por enquanto*)
-  | FNeg: forall n f, 
-                     FormulaSet n f -> (*mudar o n ?*)
-                     FormulaSet n (Neg f)
-  | FBox: forall n f, 
-                     FormulaSet n f -> 
-                     FormulaSet n (Box f)
-  | FDia: forall n f, 
-                     FormulaSet n f -> 
-                     FormulaSet n (Dia f)
-  | FAnd: forall n1 f1 n2 f2, 
-                     FormulaSet n1 f1 -> 
-                     FormulaSet n2 f2 ->
-                     FormulaSet (n1 * n2) (And f1 f2)
-  | FOr: forall n1 f1 n2 f2, 
-                     FormulaSet n1 f1 -> 
-                     FormulaSet n2 f2 ->
-                     FormulaSet (n1 + n2) (Or f1 f2)
-  | FImplies: forall n1 f1 n2 f2, (*arrumar operador entre n1 e n2*)
-                     FormulaSet n1 f1 -> 
-                     FormulaSet n2 f2 ->
-                     FormulaSet (n1 * n2) (Implies f1 f2).
-
-End Lindebaum'.
-
-(*
-############################################
-############################################
-############################################
-*)
-
-(*
-  P/ todo conjunto Gamma, se ele é consistente então existe um conjunto
-  maximal consistente Delta que contém Gamma
-*)
-Lemma Lindenbaum:
-  forall A Gamma,
-  Consistent A Gamma ->
-  exists Delta,
-  (Maximal_Consistent A Delta /\ subset Gamma Delta).
-Proof.
-  admit.
-Admitted.
-*)
--- coq/Sets.v
+++ coq/Sets.v
@@ -6,6 +6,10 @@ Inductive Singleton {T} (t: T): T -> Prop :=
 Definition Union {T} (A B: T -> Prop): T -> Prop :=
   fun t => A t \/ B t.
 
+Definition UnionOf {I T} (A: I -> T -> Prop): T -> Prop :=
+  fun t =>
+    exists i, A i t.
+
 Definition Subset {T} (A B: T -> Prop): Prop :=
   forall t: T,
   A t -> B t.
