b9a227fcc1c44a8c47a46c775f595fb09de25080
Paulo Torrens <paulotorrens@gnu.org>
2023-07-17 21:27:08 +0100

    Completeness on the way

--- coq/Completeness.v
+++ coq/Completeness.v
@@ -1,3 +1,4 @@
+Require Import Equality.
 Require Import Modal_Library Modal_Notations Deductive_System Soundness List Classical Bool Sets.
 
 (* Assume that formulas are constructively countable for now. *)
@@ -19,6 +20,33 @@ Section Properties.
     G p \/ G [! ~p !].
 
 End Properties.
+
+(* -------------------------------------------------------------------------- *)
+
+Lemma deduction_subset:
+  forall A G1 G2,
+  Subset G1 G2 ->
+  forall p,
+  deduction A G1 p -> deduction A G2 p.
+Proof.
+  induction 2.
+  - constructor 1.
+    apply H.
+    assumption.
+  - econstructor 2.
+    + eassumption.
+    + assumption.
+  - econstructor 3.
+    + apply IHdeduction1.
+      assumption.
+    + apply IHdeduction2.
+      assumption.
+  - econstructor 4.
+    apply IHdeduction.
+    assumption.
+Qed.
+
+(* -------------------------------------------------------------------------- *)
 
 Section Lindebaum.
 
@@ -85,17 +113,23 @@ Section Lindebaum.
   Lemma gamma_subset_of_max:
     Subset G Max.
   Proof.
-    unfold Subset. intros. unfold Max. unfold UnionOf. exists O. simpl. assumption. 
+    unfold Subset; intros.
+    unfold Max, UnionOf.
+    exists O; simpl.
+    assumption.
   Qed.
 
   Lemma delta_is_monotonic:
     forall n m,
-    n < m ->
+    n <= m ->
     Subset (Delta n) (Delta m).
   Proof.
-    unfold Subset. intros. induction H.
-    - simpl. apply subset_of_insert. assumption.
-    - simpl. apply subset_of_insert. assumption.
+    induction 1; intros t ?.
+    - assumption.
+    - simpl.
+      apply subset_of_insert.
+      apply IHle.
+      assumption.
   Qed.
 
   Lemma insert_contains_formula:
@@ -138,6 +172,22 @@ Section Lindebaum.
     forall D p,
     Consistent A D -> Consistent A (Insert p D).
   Proof.
+    intros.
+    destruct p.
+    - destruct classic with (Consistent A (Union (Singleton f) D)).
+      + intros p ?.
+        eapply H0.
+        apply deduction_subset with (Insert (Some f) D).
+        * intros t ?.
+          dependent destruction H2; firstorder.
+        * exact H1.
+      + admit.
+    - intros p ?.
+      apply H with p; clear H.
+      apply deduction_subset with (Insert None D).
+      + inversion 1.
+        assumption.
+      + assumption.
   Admitted.
 
   Lemma delta_is_consistent:
@@ -145,24 +195,89 @@ Section Lindebaum.
     forall n,
     Consistent A (Delta n).
   Proof.
-  Admitted.
+    induction n.
+    - simpl.
+      assumption.
+    - simpl.
+      apply insert_preserves_consistency.
+      assumption.
+  Qed.
+
+  (* If we have a derivation using Max, then, as the proof tree is finite, there
+     must be a n that is large enough for that. We don't require that it is
+     minimal. *)
+  Lemma find_largest_delta:
+    forall p,
+    (A; Max |-- p) ->
+    exists n,
+    (A; Delta n |-- p).
+  Proof.
+    intros.
+    dependent induction H.
+    (* This is a premise. We can find which n is needed! *)
+    - destruct H as (n, ?).
+      exists n.
+      econstructor 1.
+      assumption.
+    (* This is an axiom; it works for any n! *)
+    - exists 0.
+      econstructor 2.
+      + eassumption.
+      + reflexivity.
+    (* This is modus poenens. This means that we need the *largest* n between
+       the two branches! *)
+    - destruct IHdeduction1 as (n, ?); auto.
+      destruct IHdeduction2 as (m, ?); auto.
+      exists (max n m).
+      econstructor 3.
+      (* Left branch used n, can be rebuilt using (max n m)! *)
+      + apply deduction_subset with (Delta n).
+        * apply delta_is_monotonic.
+          auto with arith.
+        * eassumption.
+      (* Right branch used m, can be rebuilt using (max n m)! *)
+      + apply deduction_subset with (Delta m).
+        * apply delta_is_monotonic.
+          auto with arith.
+        * assumption.
+    (* This is necessitation; simply follows by induction! *)
+    - destruct IHdeduction as (n, ?); auto.
+      exists n.
+      econstructor 4.
+      assumption.
+  Qed.
 
   Lemma max_is_consistent:
     Consistent A G ->
     Consistent A Max.
   Proof.
-  Admitted.
+    intros ? p ?.
+    (* If Max |- p /\ ~p, then there is some n that is enough for that. *)
+    edestruct find_largest_delta as (n, ?); eauto.
+    (* Given that we know such n, Delta n is consistent as well. *)
+    assert (Consistent A (Delta n)).
+    - apply delta_is_consistent.
+      assumption.
+    - (* Since Delta n is consistent, it's a contradiction that we have both
+         p and ~p in it. *)
+      apply H2 with p.
+      assumption.
+  Qed.
 
   Theorem lindebaum:
     Consistent A G ->
     exists D,
     Consistent A D /\ Maximal D /\ Subset G D.
   Proof.
-    intros. exists Max. split.
-    - apply max_is_consistent. assumption.
+    intros.
+    exists Max; split.
+    - apply max_is_consistent.
+      assumption.
     - split.
-      + unfold Maximal. apply max_is_maximal.
-      + unfold Subset. apply gamma_subset_of_max.
+      + unfold Maximal.
+        apply max_is_maximal.
+      + unfold Subset.
+        apply gamma_subset_of_max.
   Qed.
 
 End Lindebaum.
